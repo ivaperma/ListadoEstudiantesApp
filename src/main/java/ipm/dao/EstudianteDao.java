04453bbbab586e012ff20b7d9caed96594032961
ivaperma <ivanpm89@gmail.com>
2024-02-05 18:46:18 +0100

    Crud completo

--- src/main/java/ipm/dao/EstudianteDao.java
+++ src/main/java/ipm/dao/EstudianteDao.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static ipm.conexion.Conexion.getConexion;
 
 public class EstudianteDao {
-    public List<Estudiante> listar() {
+    public List<Estudiante> listarEstudiante() {
         List<Estudiante> estudiantes = new ArrayList<>();
         // Prepara la sentencia de SQL que ejecutamos a la base de datos
         PreparedStatement ps;
@@ -46,10 +46,139 @@ public class EstudianteDao {
         return estudiantes;
     }
 
+    public boolean buscarPorId(Estudiante estudiante) {
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROM estudiante WHERE id_estudiante = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setApellido(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return false;
+    }
+
+    public boolean agregarEstudiante(Estudiante estudiante) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "INSERT INTO estudiante(nombre, apellido, telefono, email) " +
+                "VALUES(?, ?, ?, ?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public boolean modificar(Estudiante estudiante) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "UPDATE estudiante SET nombre=?, apellido=?, telefono=?, email=? WHERE id_estudiante = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.setInt(5, estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public boolean eliminar(Estudiante estudiante) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "DELETE FROM estudiante WHERE id_estudiante = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         var estudianteDao = new EstudianteDao();
+        // Agregamos estudiante
+/*        var nuevoEstudiante = new Estudiante("Pedro", "Gómez", "555444444", "carlos@mail.com");
+        var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
+        if (agregado)
+            System.out.println("Estudiante agregado: " + nuevoEstudiante);
+        else
+            System.out.println("No se agregó el estudiante: " + nuevoEstudiante);*/
+
+        // Modificamos un estudiante ya existente
+/*        var estudianteModificado = new Estudiante(1, "Juan", "Fdz", "288888888", "juan@mail.com");
+        var modificado = estudianteDao.modificar(estudianteModificado);
+        if (modificado)
+            System.out.println("Estudiante modificado: " + estudianteModificado);
+        else
+            System.out.println("No se modificó: " + estudianteModificado);*/
+
+        // Eliminar estudiante
+        var estudianteEliminar = new Estudiante(2);
+        var eliminado = estudianteDao.eliminar(estudianteEliminar);
+        if (eliminado)
+            System.out.println("Estudiante eliminado: " + estudianteEliminar);
+        else
+            System.out.println("No se eliminó el estudiante: " + estudianteEliminar);
+
+        // Listamos los estudiantes
         System.out.println("Listado de estudiantes: ");
-        List<Estudiante> estudiantes = estudianteDao.listar();
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiante();
         estudiantes.forEach(System.out::println);
+
+        // Buscamos por id
+        var estudiante1 = new Estudiante(1);
+        System.out.println("Estudiante antes de la búsqueda: " + estudiante1);
+        var encontrado = estudianteDao.buscarPorId(estudiante1);
+        if (encontrado)
+            System.out.println("Estudiante encontrado: " + estudiante1);
+        else System.out.println("No se encontró el estudiante: " + estudiante1.getIdEstudiante());
     }
 }
